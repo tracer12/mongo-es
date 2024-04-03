c139a3bc8b65c1d276db0a497712cb939dbec08c
tracer12 <vendetta12@naver.com>
2024-04-03 22:54:17 +0900

    임시 ui 추가(겉값), 기능x

--- src/components/GuiComponent.jsx
+++ src/components/GuiComponent.jsx
@@ -9,30 +9,47 @@ import {
     Text,
 } from "@chakra-ui/react";
 import '../components_css/GuiComponent.css'
-import React from "react";
+import React, { useState } from "react";
+
+/* */
 import { LANGUAGE_VERSIONS } from "../constants.js";
 const languages = Object.entries(LANGUAGE_VERSIONS);
-const ACTIVE_COLOR = "blue.400";
 
-const GuiComponent = ({ language }) => {
+const ACTIVE_COLOR = "blue.400";
+/* */
+
+const GuiComponent = () => {
+
+    const [List, setList] = useState("test");
+    const guiList = {
+        match: "1",
+        sort: "2",
+        example: "3",
+
+    }
+    const onSelect = (exList) => {
+        setList(exList);
+    };
+    const exampleList = Object.entries(guiList);
+
     return (
         <div className="gui-button-box">
             <ChakraProvider>
                 <Menu isLazy>
-                    <MenuButton as={Button}>gui button</MenuButton>
+                    <MenuButton as={Button}>{List}</MenuButton>
                     <MenuList bg="white">
-                        {languages.map(([lang, version]) => (
+                        {exampleList.map(([exList, version]) => (
                             <MenuItem
-                                key={lang}
-                                color={lang === language ? ACTIVE_COLOR : ""}
-                                bg={lang === language ? "white" : "transparent"}
+                                key={exList}
+                                color={exList === exampleList ? ACTIVE_COLOR : ""}
+                                bg={exList === exampleList ? "white" : "transparent"}
                                 _hover={{
                                     color: ACTIVE_COLOR,
                                     bg: "green",
                                 }}
-
+                                onClick={() => onSelect(exList)}
                             >
-                                {lang}
+                                {exList}
                                 &nbsp;
                                 <Text as="span" color="black" fontSize="sm">
                                     ({version})
