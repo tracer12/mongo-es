b0edc19ca4c2a33f2becf148815f2c58e7cfc440
tracer12 <vendetta12@naver.com>
2024-04-03 22:02:59 +0900

    css className 변경

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,8 +1,4 @@
 import React from 'react'
-import { BsFillArchiveFill, BsFillGrid3X3GapFill, BsPeopleFill, BsFillBellFill, BsFillCaretRightFill }
-    from 'react-icons/bs'
-import { BarChart, Bar, Cell, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer, LineChart, Line }
-    from 'recharts';
 import CodeEditor from './CodeEditor.jsx';
 import '../components_css/Home.css'
 import ExplainModal from './ExplainModal.jsx';
@@ -11,8 +7,8 @@ import GuiComponent from './GuiComponent.jsx';
 
 function Home() {
     return (
-        <main className='btn-main-container'>
-            <div className='main-btns'>
+        <main className='button-main-container'>
+            <div className='main-buttons'>
                 <ExplainModal />
                 <SaveButton />
             </div>
